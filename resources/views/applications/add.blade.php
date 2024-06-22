618902a3bfdf64708bdb468891e815d25293f5e3
9979Gyu <yuqin1161@gmail.com>
2024-06-22 23:40:24 +0800

    success test tolak others

--- resources/views/applications/add.blade.php
+++ resources/views/applications/add.blade.php
@@ -85,7 +85,7 @@
                 <td colspan="3">
                     @if($user->roleID == 5 && $offer->user_id != $user->id)
                         <!-- is not creator and not yet apply -->
-                        @if($action="nc1" || $applied == 0)
+                        @if($action == "nc1" || $applied == 0)
                             <button type="button" class="btn btn-success" name="apply" id="apply" value="mohon"><b>Mohon</b></button>
                         <!-- applied and approved -->
                         @elseif($applied > 0)
